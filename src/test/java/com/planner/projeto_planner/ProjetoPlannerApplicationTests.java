d84f0c07bc7181d8a536c47da3838b42ed0802ff
maicon8850 <maicon.douglas3527@gmail.com>
2024-08-17 16:41:15 -0300

    Criação do projeto com as bibliotecas spring | Configuração do banco dados H2

---
+++ src/test/java/com/planner/projeto_planner/ProjetoPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planner.projeto_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
